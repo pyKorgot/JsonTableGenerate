6ffc6f6083276b29376dc6cf0f9ee520d5618555
Korgoth <shutkadada@yandex.ru>
2021-06-06 18:57:45 +0300

    Fix bug date.

--- scripts/moving.js
+++ scripts/moving.js
@@ -1,42 +1,59 @@
 // Header order
 let headers = ['id', 'name', 'birthday', 'salary'];
 
-function changeHeaders(head, firstVar, secondVar){
+function changeHeaders(head, firstVar, secondVar) {
     // Change array headers
     let tmp = headers[secondVar];
     headers[secondVar] = head;
     headers[firstVar] = tmp;
 }
 
-function changeColumn(col, firstVar, secondVar){
+function changeColumn(col, firstVar, secondVar, iter, rows) {
     // Change position column
-    let tmp, tmpClass;
+    let tmp, tmpClass, tmpDate, dateField1 = false,
+        dateField2 = false;
+
+
+    if (headers[firstVar] == 'birthday' && iter != 0 && iter != rows - 1) {
+        dateField1 = true;
+        tmpDate = col[firstVar].getElementsByTagName('input')[0].value;
+
+    } else if (headers[secondVar] == 'birthday' && iter != 0 && iter != rows - 1) {
+        dateField2 = true;
+        tmpDate = col[secondVar].getElementsByTagName('input')[0].value;
+    }
 
     tmpClass = col[firstVar].className;
     tmp = col[firstVar].innerHTML;
     col[firstVar].classList.remove(tmpClass);
 
     col[firstVar].innerHTML = col[secondVar].innerHTML;
+    if (dateField2) {
+        col[firstVar].getElementsByTagName('input')[0].value = tmpDate;
+    }
     col[firstVar].classList.add(col[secondVar].className);
 
     col[secondVar].classList.remove(col[secondVar].className);
     col[secondVar].innerHTML = tmp;
+    if (dateField1) {
+        col[secondVar].getElementsByTagName('input')[0].value = tmpDate;
+    }
     col[secondVar].classList.add(tmpClass);
 
 }
 
 
-function movingColumn(head, dir){
+function movingColumn(head, dir) {
     // Find rows and items to change position
     let firstVar = headers.indexOf(head)
-    if (dir == 'left'){
+    if (dir == 'left') {
         secondVar = firstVar - 1;
-    } else if (dir == 'right'){
+    } else if (dir == 'right') {
         secondVar = firstVar + 1;
     }
-    if (secondVar == -1){
+    if (secondVar == -1) {
         secondVar = 3;
-    } else if (secondVar == 4){
+    } else if (secondVar == 4) {
         secondVar = 0;
     }
 
@@ -45,13 +62,13 @@ function movingColumn(head, dir){
         trs = table.getElementsByTagName('tr'),
         rows = trs.length;
 
-    for (let i = 0; i < rows; i++){
+    for (let i = 0; i < rows; i++) {
         if (i == 0) {
             let col = trs[i].getElementsByTagName('th');
-            changeColumn(col, firstVar, secondVar);
+            changeColumn(col, firstVar, secondVar, i, rows);
         } else {
             let col = trs[i].getElementsByTagName('td');
-            changeColumn(col, firstVar, secondVar);
+            changeColumn(col, firstVar, secondVar, i, rows);
         }
     }
     changeHeaders(head, firstVar, secondVar)
